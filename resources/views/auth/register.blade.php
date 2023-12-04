2b0f5c65ae0b6e06748f7dc909e2845cd447d50c
servant26 <alirpl16@gmail.com>
2023-12-04 14:31:07 +0800

    memperbaiki halaman register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -5,7 +5,7 @@
 <div class="register-box">
   <div class="card card-outline card-primary">
     <div class="card-header text-center">
-      <a href="{{ route('register') }}" class="h1"><b>Admin</b>LTE</a>
+      <a href="{{ route('register') }}" class="h1"><b>Register</b></a>
     </div>
     <div class="card-body">
       <p class="login-box-msg">Daftar sebagai pengguna baru</p>
